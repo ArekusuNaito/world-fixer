60dbc61aa43247def117793701015d3ea75de18c
ArekusuNaito <arekusunaito@gmail.com>
2020-02-01 14:35:49 -0600

    Added prefabs and balance object

---
+++ Assets/Code/Balance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Brotherhood/Balance", order = 1)]
+public class Balance : ScriptableObject
+{
+    [Header("Charging State")]
+    [Range(0,1)]
+    public float chargeAmount=0.2f;
+    [Header("Attacking State")]
+    [Range(0, 100)]
+    public float perfectDamage = 20f;
+    [Range(0, 100)]
+    public float okDamage = 10f;
+
+    [Header("Repair")]
+    [Range(0, 100)]
+    public float repairAmount=8f;
+}
