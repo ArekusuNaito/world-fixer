752e01817035c28a02a1a401f57b2d7ce4d7449e
ArekusuNaito <arekusunaito@gmail.com>
2020-02-01 14:36:27 -0600

    Added the GameMaster and Planet classes
    Planets can charge, get hurt and repair themselves

--- Assets/Code/GameMaster.cs
+++ Assets/Code/GameMaster.cs
@@ -1,18 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
 
 public class GameMaster : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Game Balancing")]
+    public Balance balance;
+
+    [Header("Player Planets")]
+    public Planet player1;
+    public Planet player2;
+    public Text lifeText;
+    public Text chargeText;
+
+    
+    public enum InputResult{SUCCESS,FAILURE}
+    public enum AttackPower{PERFECT,OK,MISS}
     void Start()
     {
         
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Charge(InputResult.SUCCESS,player1);
+            Attack(AttackPower.OK,player2);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Charge(InputResult.SUCCESS, player1);
+            Attack(AttackPower.OK, player2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Charge(InputResult.SUCCESS, player2);
+            Attack(AttackPower.OK, player1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Repair(player1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            Repair(player2);
+        }
+    }
+
+    
+    public void Charge(InputResult result, Planet planet)
+    {
+        //minigames dont know players
+        switch (result)
+            {
+                case InputResult.SUCCESS: //
+                    {
+                        Debug.Log("Charge Success");
+                        planet.AddCharge(balance.chargeAmount);
+                        break;
+                    }
+            }
+        }
+        
+    }
+    public void Attack(AttackPower power,Planet targetPlanet)
+    {
+        var damage = TranslateAttackPowerToFloat(power);
+        targetPlanet.Hurt(damage);
+    }
+
+    float TranslateAttackPowerToFloat(AttackPower attackPower)
+    {
+        switch (attackPower)
+        {
+            case AttackPower.PERFECT: return balance.perfectDamage;
+            case AttackPower.OK: return balance.okDamage;
+        }
+        Debug.Log("No Damage translation");
+        return 0; //default is to not receive damage
+    }   
+
+    void Repair(Planet player)
+    {
+        player.Repair(this.balance.repairAmount);
         
     }
 }
+
+
---
+++ Assets/Code/Planet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+public class Planet : MonoBehaviour
+{
+    [RangeReactiveProperty(0,100)]
+    public FloatReactiveProperty health=new FloatReactiveProperty(100);
+    [RangeReactiveProperty(0,1)]
+    public FloatReactiveProperty charge = new FloatReactiveProperty(1);
+    public enum States { CHARGING, ATTACKING, REPARING, IDLE }//WIN/LOSE
+
+    public Text healthText;
+    public Text chargeText;
+
+    void Awake()
+    {
+        this.health.SubscribeToText(healthText);
+        this.charge.SubscribeToText(chargeText);
+        
+    }
+
+    public void AddCharge(float value)
+    {
+        this.charge.Value+=value;
+    }
+
+    public void Hurt(float damage)
+    {
+        this.health.Value-=damage;
+    }
+
+    public void Repair(float value)
+    {
+        this.health.Value= this.health.Value + value >100?100:this.health.Value+value;
+        
+    }
+
+}
