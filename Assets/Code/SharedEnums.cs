60a5b1180e0c1538a332b3397f848823c59900ec
Juan <pato_evil_horn666@hotmail.com>
2020-02-01 12:44:14 -0600

    + Basics for shared enums

---
+++ Assets/Code/SharedEnums.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SharedEnums
+{
+    public enum Player { P1, P2};
+
+    public enum InputButton { A, B, X, Y };
+
+}
