a43b88316f14d4573f3a906018385d0bf8176b30
Juan <pato_evil_horn666@hotmail.com>
2020-02-01 12:44:43 -0600

    + Base for minigames

---
+++ Assets/Code/Minigames/ChargingMinigame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargingMinigame : Minigame
+{
+
+    public override void StartMinigame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StopMinigame()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Code/Minigames/Minigame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Minigame : MonoBehaviour
+{
+    public abstract void StartMinigame();
+
+    public abstract void StopMinigame();
+}
