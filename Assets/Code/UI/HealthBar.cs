69b081933dacfc9de2cf5818a4e46d068e652704
ArekusuNaito <arekusunaito@gmail.com>
2020-02-02 03:09:01 -0600

    Added the Healthbar foundations
    The GameMaster is now a singleton and adds the audiodatabase so it can be called in other places

---
+++ Assets/Code/UI/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class HealthBar : MonoBehaviour
+{
+    public Image fillImage;
+
+    void Awake()
+    {
+        fillImage.fillAmount=1;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float newValue = fillImage.fillAmount-0.2f;
+            fillImage.DOFillAmount(newValue,0.4f).SetEase(Ease.OutBounce);
+        }
+    }
+}
